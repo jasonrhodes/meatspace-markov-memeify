ff44602eba7a62b243b3c8ebf92d0c36385818ed
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-08-27 10:20:16 -0400

    Fix bug that kept successive POSTs from working

--- index.js
+++ index.js
@@ -19,6 +19,46 @@ app.get('/', function(req, res) {
   res.sendFile( __dirname + '/index.html');
 });
 
+function setFont(size, face) {
+  return size + "px " + face;
+};
+
+function sizeTextToFit(ctx, text, img, options) {
+
+  var size = options.start || img.width / 2;
+  var face = options.face || "Impact";
+  var pad = options.pad || 60;
+
+  console.log("font size", size);
+
+  ctx.font = setFont(size, face);
+
+  var info = ctx.measureText(text);
+
+  // console.log(info.width, img.width - pad, size);
+
+  while (info.width >= (img.width - pad)) {
+    size -= 5;
+    ctx.font = setFont(size, face);
+    info = ctx.measureText(text);
+  }
+
+  return { size: size, width: info.width };
+
+}
+
+function createMemeContext(canvas, options) {
+
+  var ctx = canvas.getContext('2d');
+
+  ctx.textAlign = options.align || 'left';
+  ctx.textBaseline = options.baseline || 'bottom';
+  ctx.fillStyle = options.color || "#ffffff";
+
+  return ctx;
+
+}
+
 
 app.post('/service', function(req, res) {
 
@@ -31,22 +71,19 @@ app.post('/service', function(req, res) {
     if (error) return console.log(error);
 
     var text = phrase.toUpperCase();
+    console.log("Text to add:", text);
 
     var canvas = new Canvas(img.width, img.height);
-    var ctx = canvas.getContext('2d');
-    ctx.textAlign = 'left';
-    ctx.textBaseline = 'bottom';
-    ctx.fillStyle = "#ffffff";
+    var ctx = createMemeContext(canvas, {
+      align: "left",
+      baseline: "bottom",
+      color: "#ffffff"
+    });
 
-    var fontsize = img.width;
-    ctx.font = fontsize + 'px Impact';
-    var textInfo = ctx.measureText(text);
-
-    while (textInfo.width >= (img.width - 60)) {
-      fontsize -= 5;
-      ctx.font = fontsize + 'px Impact';
-      textInfo = ctx.measureText(text);
-    }
+    var textInfo = sizeTextToFit(ctx, text, img, {
+      pad: 60,
+      face: "Impact"
+    });
 
     ctx.drawImage(img, 0, 0, img.width, img.height);
     ctx.fillText(text, (img.width - textInfo.width) / 2, img.height - 20);
